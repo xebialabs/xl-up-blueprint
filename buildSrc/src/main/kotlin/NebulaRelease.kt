dc1393c255829d482c6d21e2b618265ea5c9d92a
d-venkat <85864739+d-venkat@users.noreply.github.com>
2025-10-31 21:17:25 +0530

    E-19829 Upgrade to gradle 9.0.0 (#171)

--- buildSrc/src/main/kotlin/NebulaRelease.kt
+++ buildSrc/src/main/kotlin/NebulaRelease.kt
@@ -1,17 +1,19 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.extra
+import org.gradle.process.ExecOperations
+import javax.inject.Inject
 
-open class NebulaRelease : DefaultTask() {
+open class NebulaRelease @Inject constructor(private val execOperations: ExecOperations) : DefaultTask() {
 
     @TaskAction
     fun doRelease() {
         val version = project.extra.get("releasedVersion")
         project.logger.lifecycle("Releasing version is: $version")
 
-        project.exec {
-            executable("./gradlew")
-            args(
+        execOperations.exec {
+            executable = "./gradlew"
+            args = listOf(
                 "build", "uploadArchives", "-Prelease.version=$version", "final",
                 "-Prelease.ignoreSuppliedVersionVerification=true"
             )
